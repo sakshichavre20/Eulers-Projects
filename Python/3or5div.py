0913039ec628f258631026b1694f194dc57e493a
sakshichavre20 <sakshichavre@gmail.com>
2021-04-04 17:14:46 +0530

    added counter

--- Python/3or5div.py
+++ Python/3or5div.py
@@ -1,6 +1,9 @@
 print("program find sum of numbers divisible by 3 or 5 upto 1000")
 sum=0
+list1=[]
 for i in range(1000):
     if (i%3==0 or i%5==0):
         sum=sum+i
-print(sum)        
+        list1.append(i)
+print(sum) 
+print("number of elements divisible by 3 or 5 upto 1000: ",len(list1))       
