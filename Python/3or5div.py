25a5abea2c7addf99ce6630b88b09b3650071927
sakshichavre20 <sakshichavre@gmail.com>
2021-04-05 17:53:26 +0530

    added user input

--- Python/3or5div.py
+++ Python/3or5div.py
@@ -1,7 +1,9 @@
-print("program find sum of numbers divisible by 3 or 5 upto 1000")
+print("program find sum of numbers divisible by 3 or 5 from the given input range by user")
 sum=0
+i=int(input("Enter number from where you want sum of numbers divisible by 3 or 5"))
+j=int(input("Enter number upto where you want sum of numbers divisible by 3 or 5"))
 list1=[]
-for i in range(1000):
+for i in range(j):
     if (i%3==0 or i%5==0):
         sum=sum+i
         list1.append(i)
