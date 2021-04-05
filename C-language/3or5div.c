078b13fb5b9282b14c9c291e9047e465d4775523
sakshichavre20 <sakshichavre@gmail.com>
2021-04-05 19:50:19 +0530

    added user input

--- C-language/3or5div.c
+++ C-language/3or5div.c
@@ -1,12 +1,20 @@
-#include<stdio.h>
-int main(){
-    int i,sum=0,count=0;
-    for(i=1;i<1000;i++){
-        if(i%3==0 || i%5==0){
-            sum=sum+i;
+#include <stdio.h>
+
+int main()
+{
+    int n, num1, num2, sum = 0, count = 0;
+    printf("Enter 1st number : \n");
+    scanf("%d", &num1);
+    printf("Enter 2nd number : \n");
+    scanf("%d", &num2);
+    for (n = num1; n < num2; n++)
+    {
+        if (n % 3 == 0 || n % 5 == 0)
+        {
+            sum = sum + n;
             count++;
         }
     }
-    printf("%d The sum of numbers divisible by 3 or 5 upto 1000: \n",sum);
-    printf("%d the count of numbers divisible by 3 or 5 upto 1000: \n",count);
+    printf("The sum of numbers : %d \n", sum);
+    printf("The count of numbers : %d \n", count);
 }
