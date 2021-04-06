82053536d52e0acdca92b2058ad2490703127ac8
sakshichavre20 <sakshichavre@gmail.com>
2021-04-06 12:10:19 +0530

    testing

---
+++ Python/test1.py
@@ -0,0 +1,9 @@
+num=int(input("enter a number"))
+n1 , n2 = 0,1
+count=0
+if (num<0):
+    print("enter a positive number")
+elif(num==1):
+    print("Fibonacci of given numer is : 1")    
+else:
+    Fibonacci()
