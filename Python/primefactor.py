58e852309f54b6cef61c711860e9c450834e2cfb
sakshichavre20 <sakshichavre@gmail.com>
2021-04-10 08:44:49 +0530

    added max list

--- Python/primefactor.py
+++ Python/primefactor.py
@@ -1,6 +1,9 @@
 num=int(input("enter a number: "))
+list1=[]
 print("the values are")
 for i in range (1,num):
     if(num%i==0):
         print(i) 
-       
+        list1.append(i)
+print(list1)     
+print(max(list1))
