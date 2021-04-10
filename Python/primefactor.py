9d8360e13259839375151832d8a252bb877a6bb2
sakshichavre20 <sakshichavre@gmail.com>
2021-04-11 00:03:35 +0530

    [ADD] Euliers problem 3 solution
    it has list of factor , list of prime factors and highest primefactor

--- Python/primefactor.py
+++ Python/primefactor.py
@@ -1,10 +1,34 @@
-num=int(input("enter a number: "))
-list1=[]
-print("the values are")
-for i in range (2,num):
-    if(num%i==0):
-        print(i) 
-        list1.append(i)
-print(list1)
-print(max(list1))
-      
+import math 
+num = 600851475143
+list1 =[]
+list2=[]
+# find all factors 
+def factors(num):
+    global list1
+    global n1
+    for i in range(2, int(math.sqrt(num))):
+        if (num % i == 0):           
+            list1.append(i)
+    print(list1)
+    
+    
+factors(num)
+
+
+# check for prime factors
+
+def prime(num):
+    global list2
+    for i in list1:
+        prime= True
+        for j in range (2,i):
+            if i%j==0:
+                prime= False
+        if prime == True :
+            list2.append(i)
+    print(list2)
+
+prime(num)
+            
+# print largest prime factor
+print(max(list2))   
