ad70c1639b6f9a39eef364222916d508d0bdabe9
sakshichavre20 <chavresakshi@gmail.com>
2021-04-11 18:41:31 +0530

    [ADD] solution for euler 4

--- C-language/palindrome.c
+++ C-language/palindrome.c
@@ -1,32 +1,3 @@
-/*def isPalindrome(num):
-    return str(num) == str(num)[::-1]
-
-
-def largest(min, max):
-    z = 0
-    for i in range(max, min, -1):
-        for j in range(max, min, -1):
-            if isPalindrome(i*j):
-                if i*j > z:
-                    z = i*j
-    return z
-
-
-print(largest(100, 999))*/
-#include<stdio.h>
-#include<conio.h>
-#include<string.h>
-
-int main ()
-{
-int pali;
-int max , min, num;
-pali = palindrome(max,min);
-printf("%d",pali);
-
-}
-int palindrome(num) {
-    int i;
-    for 
-    return str(num) == str(num-i-1);
-}
+// first find all products 
+// search for palindrome
+//print largest palindrome
