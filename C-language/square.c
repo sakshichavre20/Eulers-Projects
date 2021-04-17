c3f805a21bb5106f2c788c5336a91edf189fd08e
sakshichavre20 <chavresakshi@gmail.com>
2021-04-17 15:13:54 +0530

    [ADD] solution for euler6
    this code finds the substraction of square of sum addition of all numbers upto 100 and sum of squares of all numbers upto 100

---
+++ C-language/square.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<Conio.h>
+
+int main(){
+    int i,sq,j,sum1=0,sum2=0,sq1,result;
+    for (i=1;i<101;i++){
+        sq=i*i;
+        sum1=sum1+sq;    
+    }
+    for(i=1;i<101;i++){
+        sum2=sum2+i;
+    }
+    sq1=sum2*sum2;
+
+result=sq1-sum1;
+printf("%d ", result);
+
+return 0;
+}
